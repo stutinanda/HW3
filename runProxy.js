920d7f4f50e6bc51e4636912f57d28589124e329
Stuti Nanda <stuti.ncsu@gmail.com>
2016-11-17 01:50:20 +0000

    edit proxy

--- runProxy.js
+++ runProxy.js
@@ -1,10 +1,10 @@
 var http = require('http'),
     httpProxy = require('http-proxy');
 // REDIS
-//var redis = require('redis')
-//var client = redis.createClient(6379, 'redis') 
+var redis = require('redis')
+var client = redis.createClient(6379, 'redis') 
 var server_index = 0;
-var app_ip = process.env.APPL_IP;
+//var app_ip = process.env.APPL_IP;
 // 
 // Create a proxy server with custom application logic 
 // 
@@ -19,19 +19,19 @@ var server = http.createServer(function(req, res) {
   // You can define here your custom logic to handle the request 
   // and then proxy the request. 
 	
-	/*client.llen("listserver", function (err, server_count){
+	client.llen("listserver", function (err, server_count){
 		if ((server_index + 1) > server_count){
 			server_index = 0;
 		}
-	})*/
+	})
 
 	setTimeout(function(){
-		//client.lindex("listserver", server_index, function(err, value){
+		client.lindex("listserver", server_index, function(err, app_ip){
 			server_index += 1;
 			value = 3000;
   			proxy.web(req, res, { target: 'http://' + app_ip + ':' + value });
                 	console.log('Forwarding request to http://' + app_ip + ':' + value);
-        	//})
+        	})
 	}, 1500);
 
 });
